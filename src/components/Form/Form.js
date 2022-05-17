d2458aba0918dff948996b60aa7d5d3b5d18b5f7
Lautaro Recio <reciolauti@gmail.com>
2022-05-17 12:02:03 -0300

    seccionServicios

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,7 +8,6 @@ export default function Form(){
     const expresiones = {
         nombre: /^[a-zA-ZÀ-ÿ\s]{4,40}$/, // Letras y espacios, pueden llevar acentos.
         gmail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-        mensaje: /^[a-zA-ZÀ-ÿ\s\!\¡\¿\?]{4,200}$/, // Letras y espacios, pueden llevar acentos.
     };
     function reset(){
         setNombre("");setApellido("");setMail("");setMensaje("");
@@ -22,33 +21,35 @@ export default function Form(){
             <form action="">
                 <div className='divGris'>
                 
-                        <label htmlFor="" style={{color: (nombreForm=== "" ? `` :( expresiones.nombre.test(nombreForm)===false?"red":"green")) }}> {nombreForm=== "" ? ` Nombre` :( expresiones.nombre.test(nombreForm)===false?"Nombre invalido":"Nombre Valido")}</label>
-                        <input type="text" value={nombreForm} onChange={nombreChange}/>
+                        <input type="text"  style={{borderBottom: (nombreForm=== "" ? `` :( expresiones.nombre.test(nombreForm)===false?"1px solid red": (nombreForm=== " " ?" ":" 1px solid green"))) }} value={nombreForm} onChange={nombreChange}/>
+                        <p className='pBlanco' style={{color: (nombreForm=== "" ? `` :( expresiones.nombre.test(nombreForm)===false?"red":"green")) }}> {nombreForm=== "" ? ` Nombre` :( expresiones.nombre.test(nombreForm)===false?"Nombre invalido":"Nombre Valido")}</p>
                     
                 </div>
 
                 <div className='divGris'>
                 
-                        <label htmlFor="" style={{color: (apellido=== "" ? `` :( expresiones.nombre.test(apellido)===false?"red":"green")) }}>{apellido=== "" ? `Apellido` :( expresiones.nombre.test(apellido)===false?"Apellido invalido":"Apellido Valido")}</label>
-                        <input type="text" value={apellido} onChange={apellidoChange}/>
+                        <input type="text" style={{borderBottom: (apellido=== "" ? `` :( expresiones.nombre.test(apellido)===false?"1px solid red":(apellido=== " " ?" ":" 1px solid green"))) }} value={apellido} onChange={apellidoChange}/>
+                        <p className='pBlanco' style={{color: (apellido=== "" ? `` :( expresiones.nombre.test(apellido)===false?"red":"green")) }}>{apellido=== "" ? `Apellido` :( expresiones.nombre.test(apellido)===false?"Apellido invalido":"Apellido Valido")}</p>
                     
                 </div>
 
                 <div className='divGris'>
                 
-                        <label htmlFor="" style={{color: (mail=== "" ? `` :( expresiones.gmail.test(mail)===false?"red":"green")) }}>{mail=== "" ? `Corre Electronico` :( expresiones.gmail.test(mail)===false?"Corre Electronico invalido":"Corre Electronico Valido")}</label>
-                        <input type="mail" value={mail} onChange={mailChange}/>
+                        <input type="mail" style={{borderBottom: (mail=== "" ? `` :( expresiones.gmail.test(mail)===false?"1px solid red":(mail=== " " ?" " : " 1px solid green"))) }} value={mail} onChange={mailChange}/>
+                        <p className='pBlanco' style={{color: (mail=== "" ? `` :( expresiones.gmail.test(mail)===false?"red":"green")) }}>{mail=== "" ? `Corre Electronico` :( expresiones.gmail.test(mail)===false?"Corre Electronico invalido":"Corre Electronico Valido")}</p>
                     
                 </div>
                 
                 <div className='divGris'>
                    
-                        <label htmlFor="" style={{color: (mensaje=== "" ? `` :( expresiones.mensaje.test(mensaje)===false?"red":"green")) }}>{mensaje=== "" ? `Mensaje` :( expresiones.mensaje.test(mensaje)===false?"Mensaje invalido":"Mensaje Valido")}</label>
                         <input className='textArea' type="text-area" value={mensaje} onChange={mensajeChange}/>
+                        <p className='pBlanco'>Mensaje</p>
                     
                 </div>
-                <button type="submit" >Enviar</button>
-                <button type="reset" onClick={()=>reset()}>Limpiar</button>
+                <div className="botones"> 
+                    <button type="submit" style={{backgroundColor: ((expresiones.gmail.test(mail)===false) || (expresiones.nombre.test(apellido)===false) || (expresiones.nombre.test(nombreForm)===false))?"red":"#f36921"}} disabled={((expresiones.gmail.test(mail)===false) || (expresiones.nombre.test(apellido)===false) || (expresiones.nombre.test(nombreForm)===false))} >Consultar</button>
+                    <button type="reset" onClick={()=>reset()}>Limpiar</button>
+                </div>
             </form>
         </>
     )
--- src/components/Servicios/Servicios.js
+++ src/components/Servicios/Servicios.js
@@ -1,12 +1,34 @@
 import React from "react";
 import './Servicios.css';
 function Servicios() {
+    const servicios=[
+        {id:1,titulo:"ASESORÍAS 1:1",color:"#dbbeff"},
+        {id:2,titulo:"ASESORÍAS GRUPALES",color:"#f36921"},
+        {id:3,titulo:"DISEÑO DE IDENTIDAD DIGITAL ",color:"#fcdb38"},
+        {id:4,titulo:"GENERACIÓN DE CONTENIDO PARA MARCAS",color:"#faaae5"},
+    ]
     return(
-        <section className="servicios" id="servicios">
-            <span className="servicios-title">
-                Servicios
-            </span>
-        </section>
+        <div className="servicios" id="servicios">
+            <div className="Explicacion">
+                <span>Servicios</span>
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quo sunt esse totam. Itaque distinctio voluptate placeat pariatur, aliquam, odio eligendi error molestiae illo ipsam dolore. Pariatur culpa mollitia nemo.</p>
+                <button>Solicitar</button>
+                <img src='../images/ICONOGRAFIA BYAILEN-03.png' alt="" />
+            </div>
+            <div className="servs">
+                {servicios.map((servicio)=>{
+                    return(
+                        <div className="redondeo" key={servicio.id} style={{backgroundColor: servicio.color}}>
+                            <p>V</p>
+                            <h3>{servicio.titulo}</h3>
+                            <button onClick={console.log("FUNCIONO")}>Consultar</button>
+                        </div>
+                    )
+                    })
+                
+                }
+            </div>
+        </div>
     )
 }
 
